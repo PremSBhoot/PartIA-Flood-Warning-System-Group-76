ab81ad7a14aeeb26466b6869264910bc1e94c804
PremSBhoot <prem.bhoot@yahoo.com>
2022-02-07 11:30:18 +0000

    Tasks 1B, C, D updated demonstration program - just need to add tests :)

--- Task1B.py
+++ Task1B.py
@@ -1,9 +1,22 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
-stations_input = build_station_list()
-coord = (52.2053, 0.1218) #coordinate of cambridge city centre
-stations_sorted_by_distance = stations_by_distance(stations_input, coord)
+def run():
+    #Build Station Input List
+    stations_input = build_station_list()
 
-print(stations_sorted_by_distance[:10])
-print(stations_sorted_by_distance[-10:])
+    #coordinate of cambridge city centre
+    coord = (52.2053, 0.1218) 
+    stations_sorted_by_distance = stations_by_distance(stations_input, coord)
+
+    #print 10 closest stations from coordinate specified
+    print("10 closest stations from ", coord)
+    print(stations_sorted_by_distance[:10])
+
+    #print 19 furthest stations from coordinate specified
+    print("10 furthest stations from ", coord)
+    print(stations_sorted_by_distance[-10:])
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- Task1C.py
+++ Task1C.py
@@ -1,9 +1,19 @@
 from distutils.command.build import build
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
+def run():
+    #Build Station Input List
+    stations_input = build_station_list()
 
-stations_input = build_station_list()
-coord = (52.2053, 0.1218) #coordinate of cambridge city centre
-stationsWithinRadius = stations_within_radius(stations_input, coord, 10)
+    #coordinate of cambridge city centre
+    coord = (52.2053, 0.1218) 
 
-print(sorted(station.get_stationName() for station in stationsWithinRadius)) 
+    #retrives stations within radius of 10 from coordinate
+    stationsWithinRadius = stations_within_radius(stations_input, coord, 10)
+
+    print("Prints sorted list in ascending order of stations within the radius")
+    print(sorted(station.get_stationName() for station in stationsWithinRadius)) 
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
