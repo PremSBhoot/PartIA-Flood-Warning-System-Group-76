8980fadd8bb9754b924857711890b0fcdeeb03a3
PremSBhoot <prem.bhoot@yahoo.com>
2022-02-10 10:07:34 +0000

    test temp

--- test_geo.py
+++ test_geo.py
@@ -3,6 +3,7 @@
 from attr import has
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
 
 
 def test_stations_by_distance():
@@ -11,7 +12,22 @@ def test_stations_by_distance():
     stations_sorted_by_distance = stations_by_distance(stations_list, coord)
 
     assert "Cambridge Jesus Lock" in stations_sorted_by_distance[0][0]
-    assert 0.8402364350834995 == stations_sorted_by_distance[0][2]
+    assert round(stations_sorted_by_distance[0][2] - 0.8402364350834995, 5) == 0.0
+
+    sublist = stations_sorted_by_distance[:10]
+    assert len(sublist) == 10
+
+    print(stations_sorted_by_distance[0][2])
+    print(stations_sorted_by_distance[1][2])
+    for i in range (len(sublist) - 1):
+        print(i)
+        assert sublist[i][2] < sublist[i + 1][2]
+
+
+"""def test_rivers_by_station_number():
+    stations_list = build_station_list()
+    assert len(rivers_by_station_number()) >= 10
+    assert rivers_by_station_number[0][1] >= rivers_by_station_number[9][1]"""
 
     
 
